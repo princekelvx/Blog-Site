5ded611005730b983beda164f7dc68d1d8632763
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-03-30 12:16:03 -0700

    Display posts on the home page

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -17,7 +17,7 @@ const PostWidget = ({ categories, slug }) => {
     }
   }, [slug])
   
-  console.log(relatedPosts)
+  // console.log(relatedPosts)
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
